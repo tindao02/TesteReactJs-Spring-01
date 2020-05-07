ee119f67b22afb55c1f1695f4b0c239a845c801a
Manoel Tindão <timdao02@hotmail.com>
2020-05-07 13:31:18 -0300

    Criação do primeiro projeto de React JS com Spring

---
+++ src/main/java/com/tindao/greglturnquist/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.tindao.greglturnquist;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PayrollApplication.class);
+	}
+
+}
---
+++ src/main/java/com/tindao/greglturnquist/payroll/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.tindao.greglturnquist.payroll;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Long>
+{
+
+}
